caca89104b9f3d63cd8e5b78021df9ef6fd957d7
Till Hoffmann <till.hoffmann@netlight.com>
2023-01-31 17:40:27 +0100

    remove useless root cmd description

--- cmd/root.go
+++ cmd/root.go
@@ -30,16 +30,8 @@ var (
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use: "temingo",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:  "temingo",
 	Args: cobra.NoArgs,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) {
 		var (
 			err error
